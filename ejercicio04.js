d5181994998ae208a1f5a0666a9bdb0522ff4fdd
iglesias-juan <iglesias.juan@outlook.es>
2024-10-01 19:36:19 +0200

    Feat: commit 1

---
+++ ejercicio04.js
@@ -0,0 +1,22 @@
+const cartoons = [
+  { name: 'Mickey Mouse', debut: 1928 },
+  { name: 'Bugs Bunny', debut: 1938 },
+  { name: 'SpongeBob SquarePants', debut: 1999 },
+  { name: 'Tom and Jerry', debut: 1940 },
+  { name: 'Scooby-Doo', debut: 1969 },
+  { name: 'The Flintstones', debut: 1960 },
+  { name: 'Batman: The Animated Series', debut: 1992 },
+  { name: 'The Simpsons', debut: 1989 },
+  { name: 'Pokémon', debut: 1997 },
+  { name: "Dexter's Laboratory", debut: 1996 }
+]
+
+let oldestCartoon = cartoons[0]
+
+for (let i = 0; i < cartoons.length; i++) {
+  if (cartoons[i].debut < oldestCartoon.debut) {
+    oldestCartoon = cartoons[i]
+  }
+}
+
+console.log('Serie de dibujos más antigua:', oldestCartoon.name)
